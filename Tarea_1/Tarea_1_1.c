f5230e3e3e7945ece9b3afdb9f3f6fb8ac7a300f
Juan David Montenegro Lopez <jumontenegrol@unal.edu.co>
2025-09-01 09:38:56 -0500

    Actualizar Tarea_1_1.c
    estructura para el archivo que Lee los datos

--- Tarea_1/Tarea_1_1.c
+++ Tarea_1/Tarea_1_1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+struct Usuario {
     char nombre[30];
     char apellido[30];
     int edad;
     int estatura;
+}
+int main() {
+    struct Usuario u;
 
     // Abrir archivo en modo lectura
     FILE *archivo = fopen("datos.txt", "r");
@@ -15,13 +18,13 @@ int main() {
     }
 
     // Leer los datos del archivo
-    fscanf(archivo, "%s %s %d %d", nombre, apellido, &edad, &estatura);
+    fscanf(archivo, "%s %s %d %d", u.nombre, u apellido, &u.edad, &u.estatura);
 
     fclose(archivo);
 
-    printf("Hola %s %s\n", nombre, apellido);
-    printf("Tu edad es: %d\n", edad);
-    printf("Tu estatura es: %d cm\n", estatura);
+    printf("Hola %s %s\n", u.nombre, u.apellido);
+    printf("Tu edad es: %d\n", u.edad);
+    printf("Tu estatura es: %d cm\n",u.estatura);
 
     return 0;
 }
